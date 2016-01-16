571635e13990d94f8cf6d3c26fd41628cecf8f4f
gmsjy <ZTXYYCX@hotmail.com>
2016-01-16 18:06:34 +0800

    去掉了2>/dev/null

--- testhttphead.sh
+++ testhttphead.sh
@@ -7,26 +7,34 @@ echo $*
 echo $0
 echo $1
 
-if(($# < 2));
-then
+if (( $#<2));then
     echo "usage:$0 host port";
     exit 1;
 fi
 
-exec 6<>/dev/tcp/$1/$2 2>/dev/null;
+exec 6<>/dev/tcp/$1/$2 ;
 #打开host的PORT 可读写的Socket连接，与文件描述符6相连
 
-if(($?!=0))；then
+echo $2 $?
+
+if (($?!=0));then
     echo "open $1 $2 erro!";
+    echo "hini";
     exit 1;
 fi
 #如果打开失败，#?返回不为0，终止退出
+echo "hi"
 
-echo -e "HEAD / HTTP/1.1\r\n\r\n" >&6;
+echo -e "HEAD / HTTP/1.1\n\n\n\n\n" >&6;
 #将HEAD信息发出给Socket
+
+echo $?
+echo "hi"
 
 cat <&6;
 #从Socket读取返回信息
+
+echo "Hello"
 
 exec 6<&-;
 exec 6>&-;
